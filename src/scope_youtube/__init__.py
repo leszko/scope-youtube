72df505c682dea3e7968cd731d04d2f48cce20df
Rafał Leszko <rafal@livepeer.org>
2026-04-28 11:12:40 +0200

    Add info about source

--- src/scope_youtube/__init__.py
+++ src/scope_youtube/__init__.py
@@ -2,6 +2,11 @@
 
 from .plugin import YouTubePlugin
 
+# Read by Scope to decide whether the plugin should run locally
+# (source-kind) or in the cloud. Pre-install: AST-parsed from this
+# file. Post-install: read from the imported module.
+__scope_kind__ = "source"
+
 plugin = YouTubePlugin()
 
-__all__ = ["plugin", "YouTubePlugin"]
+__all__ = ["plugin", "YouTubePlugin", "__scope_kind__"]
